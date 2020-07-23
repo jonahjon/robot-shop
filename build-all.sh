ae040cdacf661651392e4551e99e7e8ba10c4b07
Apoorva Kulkarni <kuapoorv@amazon.com>
2020-07-23 12:09:42 -0700

    update Makefiles and build scripts for ECR module

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-for name in cart catalogue dispatch load-gen mongo mysql ratings shipping payment user web; do make -C $name ci-release  ; done
+COMMIT_SHA=${CODEBUILD_SOURCE_VERSION:-$(git rev-parse HEAD)}
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-west-2}
+ACCOUNT=${ACCOUNT:-164382793440}
+
+for name in cart catalogue dispatch load-gen mongo mysql ratings shipping payment user web; do
+    make -C $name ci-release \
+        TAG=$COMMIT_SHA \
+        AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+        ACCOUNT=$ACCOUNT
+done
 
 
 # Build all X-ray services
